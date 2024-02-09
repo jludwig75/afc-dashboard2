e210ddb95e31bb09be47f6e2ca22db41561ea31c
Jonathan Ludwig <jr.ludwig@gmail.com>
2024-02-08 20:53:50 -0700

    Created repo

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <VLayout>
+    <VApp>
+      <VAppBar
+        app
+        color="primary"
+      >
+        <VAppBarTitle>
+          AFC Backup Dashboard
+        </VAppBarTitle>
+      </VAppBar>
+      <VMain>
+        <VCard>
+          <VCardTitle>
+            American Fork Canyon Links
+          </VCardTitle>
+          <VCardText>
+            <VList>
+              <VListItem>
+                <VListItemTitle>
+                  <a href="https://www.weather.gov/wrh/timeseries?site=TIMU1">Timpanogos Divide Snow Depth and Temperature (National Weather Service)</a>
+                </VListItemTitle>
+                <VListItemTitle>
+                  <a href="https://snoflo.org/report/snow/utah/timpanogos-divide/">Timpanogos Divide Snow Depth and Temperature (SNOFLO)</a>
+                </VListItemTitle>
+                <VListItemTitle>
+                  <a href="https://utahavalanchecenter.org/forecast/provo">Avalanche Forecase - Provo Area</a>
+                </VListItemTitle>
+                <VListItemTitle>
+                  <a href="https://staging.udottraffic.utah.gov/">UDOT Traffic Cameras</a>
+                </VListItemTitle>
+              </VListItem>
+            </VList>
+          </VCardText>
+        </VCard>
+      </VMain>
+    </VApp>
+  </VLayout>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import VueMobileDetection from "vue-mobile-detection";
+import { createApp } from 'vue'
+import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import { loadFonts } from './plugins/webfontloader'
+
+loadFonts()
+
+createApp(App)
+  .use(vuetify)
+  .use(VueMobileDetection)
+  .mount('#app')
