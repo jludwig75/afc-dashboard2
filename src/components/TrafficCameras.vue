73fa5600e4e27e0d708afdbc7b0ceddc000f8272
Jonathan Ludwig <jr.ludwig@gmail.com>
2024-02-09 21:13:49 -0700

    Add remaining cameras

--- src/components/TrafficCameras.vue
+++ src/components/TrafficCameras.vue
@@ -11,7 +11,7 @@
       <VImg
         v-for="image in images"
         :key="image"
-        :class="$isMobile() ?  '' : 'mx-12'"
+        :class="$isMobile() ?  'mb-5' : 'mx-12 mb-8'"
         :src="image.link"
         :width=" $isMobile() ? '' : '400'"
         cover
@@ -37,6 +37,24 @@ export default {
       },
       { name: 'Tibble Fork',
         link: 'https://udottraffic.utah.gov/1_devices/aux17726.jpeg',
+      },
+      { name: 'Tibble For Mile 1.72',
+        link: 'https://udottraffic.utah.gov/1_devices/aux18005.jpeg',
+      },
+      { name: 'Tibble For Mile 0.88',
+        link: 'https://udottraffic.utah.gov/1_devices/aux17725.jpeg',
+      },
+      { name: 'Road Fork',
+        link: 'https://udottraffic.utah.gov/1_devices/aux17728.jpeg',
+      },
+      { name: 'Timpanogos Cave',
+        link: 'https://udottraffic.utah.gov/1_devices/aux17729.jpeg',
+      },
+      { name: 'Fee Booth',
+        link: 'https://udottraffic.utah.gov/1_devices/aux17730.jpeg',
+      },
+      { name: 'Canyon Mouth',
+        link: 'https://udottraffic.utah.gov/1_devices/aux18049.jpeg',
       },
     ]
   }),
